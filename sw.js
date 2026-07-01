4e43b2bd26ffe3a6ef7ab7cdd684669ef863dac7
ki <164283577+v-kimura0123@users.noreply.github.com>
2026-07-01 13:54:45 +0900

    Publish Kimu Task Web app

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'kimu-task-v6';
+const FILES = ['./','index.html','styles.css?v=6','app.js?v=6','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
+self.addEventListener('activate', event => event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))])));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response; }).catch(()=>caches.match('./index.html')))));
