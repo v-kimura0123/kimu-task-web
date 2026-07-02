3cead5433bbbe3536ab44c2d9516ff7920d9a0b6
ki <164283577+v-kimura0123@users.noreply.github.com>
2026-07-02 10:43:45 +0900

    Refresh offline cache to v11

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kimu-task-v10';
-const FILES = ['./','index.html','styles.css?v=10','detail.css?v=10','app.js?v=10','manifest.webmanifest','icon-192.png','icon-512.png'];
+const CACHE = 'kimu-task-v11';
+const FILES = ['./','index.html','styles.css?v=11','detail.css?v=11','app.js?v=11','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))])));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response; }).catch(()=>caches.match('./index.html')))));
